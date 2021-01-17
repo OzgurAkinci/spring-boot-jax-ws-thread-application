039c959b328efa519009c91c412fc9d5f950b134
Özgür A <akincior@gmail.com>
2021-01-17 05:04:06 +0300

    Test Controller

--- client-application/src/main/java/com/ws/controller/TestController.java
+++ client-application/src/main/java/com/ws/controller/TestController.java
@@ -1,20 +1,33 @@
 package com.ws.controller;
 
+import com.project.generated.Expense;
+import com.project.generated.ExpenseService;
+import com.project.generated.GetExpensesRequest;
+import com.project.generated.GetExpensesResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
+@RequestMapping("/expense")
 @RestController
 @Component
 public class TestController {
-    @GetMapping("/executeWithoutThread")
-    public String list() {
-        /*ObjectFactory objectFactory = new ObjectFactory();
-        GetExpenseRequest a = objectFactory.createGetExpenseRequest();
-        a.setId(1);
-        GetExpense b = objectFactory.createGetExpense();*/
-        return "done";
+
+    private static Expense expense;
+    static {
+        expense = new ExpenseService().getExpensePort();
+    }
+
+    @GetMapping("/executeGetExpenses")
+    public GetExpensesResponse getExpenses() {
+        GetExpensesRequest getExpensesRequest = new GetExpensesRequest();
+        return expense.getExpenses(getExpensesRequest);
+    }
+
+    @GetMapping("/executeSaveThreadWithoutThread")
+    public GetExpensesResponse saveThreadWithoutThread() {
+        GetExpensesRequest getExpensesRequest = new GetExpensesRequest();
+        return expense.getExpenses(getExpensesRequest);
     }
 }
